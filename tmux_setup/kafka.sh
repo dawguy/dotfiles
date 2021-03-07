f2441ed3aa48adbf132cbff48ba2fa9e952cf31d
David Wright <hockeyguy411@gmail.com>
2021-03-07 09:31:17 -0500

    Kafka tmux no longer opens by default

--- tmux_setup/kafka.sh
+++ tmux_setup/kafka.sh
@@ -1,18 +1,19 @@
 tmux new-session -d -s kafka
 tmux split-window -v
-tmux select-window -t 1
 
 # Start zookeeper
+tmux select-pane -t 0
 tmux send-keys 'cd ${MY_KAFKA_HOME}'
 tmux send-keys Enter
 tmux send-keys 'bin/zookeeper-server-start.sh config/zookeeper.properties'
 tmux send-keys Enter
 
 # Start kafka server
-tmux select-window -t 2
+tmux select-pane -t 1
 tmux send-keys 'cd ${MY_KAFKA_HOME}'
 tmux send-keys Enter
 tmux send-keys 'bin/kafka-server-start.sh config/server.properties'
 tmux send-keys Enter
 
-tmux attach-session -t kafka
+echo "To see the kafka windows run the below command"
+echo "tmux a -t kafka"
