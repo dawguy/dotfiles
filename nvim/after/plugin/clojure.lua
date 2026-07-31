fb59bd7de1d7f6c010666c4130992914571d6753
dawguy <wright.david.27@gmail.com>
2026-07-31 19:58:13 -0400

    Bump up timeout. My fingers are too slow to hit ,e! in 300ms.

--- nvim/after/plugin/clojure.lua
+++ nvim/after/plugin/clojure.lua
@@ -1,7 +1,8 @@
 -- Clojure: Conjure uses <localleader> as its mapping prefix.
 vim.g.maplocalleader = ","
--- Faster key-sequence timeout so prefix collisions (e.g. ,v and ,x) don't lag.
-vim.opt.timeoutlen = 300
+-- Give multi-key chords like ,e! room to breathe. The tradeoff: keys that are
+-- prefixes of Conjure's maps (e.g. ,v and ,x) wait this long before firing.
+vim.opt.timeoutlen = 650
 
 -- Structural editing: slurp/barf/drag/raise with >, < and form text objects.
 require('nvim-paredit').setup({
